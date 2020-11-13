f1ef61b78bbd269edfcbe6731ee01a3398c01776
Diuer <diuer71@gmail.com>
2020-11-13 14:32:18 +0800

    feat: switch button of slider

---
+++ src/SliderButton.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleRight, faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
+import GlobalData from './globalData';
+
+import Tomato from './tomato';
+
+const ButtonStyle = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;  width: 90px;
+  height: 50px;
+  bottom: 35px;
+  left: -25px;
+  background-color: #fcfcfc;
+  border-radius: 25px 5px 5px 25px;
+  cursor: pointer;
+`;
+
+export default function SliderButton() {
+
+  const [isOpenedSlider, setSlider] = useState(false);
+
+  useEffect(() => {
+    GlobalData.isOpenedSlider = isOpenedSlider
+  }, [isOpenedSlider]);
+
+  return (
+    < ButtonStyle onClick={() => { setSlider(!isOpenedSlider) }
+    }>
+      <Tomato isDisabled={false} cursorKind={'pointer'} />
+      {
+        (isOpenedSlider ?
+          <FontAwesomeIcon icon={faAngleDoubleRight} size='lg' /> : <FontAwesomeIcon icon={faAngleDoubleLeft} size='lg' />)
+      }
+
+    </ButtonStyle >
+  )
+}
