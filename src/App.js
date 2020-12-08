2e858e090f57ff23445adc1395936255c0b2f22a
Diuer <diuer71@gmail.com>
2020-12-09 00:35:54 +0800

    feat: switch menu & show content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
@@ -9,7 +9,7 @@ import SliderButton from './SliderButton';
 
 const WorkSpaceStyle = styled.div`
   display: block;
-  width: 50vw;
+  width: ${(props) => props.isOpenedSlider ? '60vw' : 'calc(100vw - 80px)'};
   height: 100vh;
   background-color: #eaeaea;
 `;
@@ -18,21 +18,35 @@ const SubSpaceStyle = styled.div`
   position: relative;
   display: flex;
   background-color: #333333;
-  width: 50vw;
+  width: ${(props) => props.isOpenedSlider ? '40vw' : '80px'};
   height: 100vh;
 `;
 
 export default function App() {
+
+  const [isOpenedSlider, setSlider] = useState(true);
+  const [taskData, setTaskData] = useState([]);
+  const [nowMenu, setNowMenu] = useState('showList');
+
+  function onClickSlider() {
+    setSlider(!isOpenedSlider);
+  };
+
+  function createTask(data) {
+    console.log(data);
+    setTaskData(taskData.concat(data));
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <WorkSpaceStyle>
+        <WorkSpaceStyle isOpenedSlider={isOpenedSlider}>
           <WorkSpaceContainer />
         </WorkSpaceStyle>
-        <SubSpaceStyle>
-          <SliderButton />
-          <SliderMenu />
-          <SliderContent />
+        <SubSpaceStyle isOpenedSlider={isOpenedSlider}>
+          <SliderButton onClickSlider={onClickSlider} isOpenedSlider={isOpenedSlider} />
+          <SliderMenu nowMenu={nowMenu} setNowMenu={setNowMenu} />
+          {isOpenedSlider && <SliderContent isOpenedSlider={isOpenedSlider} nowMenu={nowMenu} createTask={createTask} taskData={taskData} />}
         </SubSpaceStyle>
       </ThemeProvider>
     </>)
